71ebd8025a29f29338e3feb735cbaf97b5f12dbc
nosu23 <raidzimmer52@gmail.com>
2023-05-02 19:52:19 +0900

    update alias

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -57,3 +57,8 @@ rm-local-git-branch() {
   git branch --color | fzf --ansi | xargs git br -D
 }
 alias rmlg=rm-local-git-branch
+
+permission-number-ls() {
+  ls -l "$@" | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf("%0o ",k);print}';
+}
+alias pnls=permission-number-ls
