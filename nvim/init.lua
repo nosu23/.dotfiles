a7e03a09b99d772a2cc242dba6769546c020815b
nosu23 <raidzimmer52@gmail.com>
2024-09-24 21:51:13 +0900

    add telescope to plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
-
+-- leaderをスペースへ設定
+vim.g.mapleader = ' '
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -28,3 +29,9 @@ require("lazy").setup("plugins", {
 
 
 vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
